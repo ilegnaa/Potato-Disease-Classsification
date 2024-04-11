8e6db51ca87ed15925dc16cf84a3de3338edbf8f
Evangeline <108733602+ilegnaa@users.noreply.github.com>
2024-04-11 12:25:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,96 +1,80 @@
-# Imporiting Necessary Libraries
 import streamlit as st
 from PIL import Image
-import io
 import numpy as np
-import tensorflow as tf
-from utils import clean_image, get_prediction, make_results
+import tensorflow.keras as keras
+import matplotlib.pyplot as plt
+import tensorflow_hub as hub
 
-# Loading the Model and saving to cache
-@st.cache(allow_output_mutation=True)
-def load_model(path):
-    
-    # Xception Model
-    xception_model = tf.keras.models.Sequential([
-    tf.keras.applications.xception.Xception(include_top=False, weights='imagenet', input_shape=(512, 512, 3)),
-    tf.keras.layers.GlobalAveragePooling2D(),
-    tf.keras.layers.Dense(4,activation='softmax')
-    ])
-
-
-    # DenseNet Model
-    densenet_model = tf.keras.models.Sequential([
-        tf.keras.applications.densenet.DenseNet121(include_top=False, weights='imagenet',input_shape=(512, 512, 3)),
-    tf.keras.layers.GlobalAveragePooling2D(),
-    tf.keras.layers.Dense(4,activation='softmax')
-    ])
-
-    # Ensembling the Models
-    inputs = tf.keras.Input(shape=(512, 512, 3))
-
-    xception_output = xception_model(inputs)
-    densenet_output = densenet_model(inputs)
-
-    outputs = tf.keras.layers.average([densenet_output, xception_output])
-
-
-    model = tf.keras.Model(inputs=inputs, outputs=outputs)
-
-    # Loading the Weights of Model
-    model.load_weights(path)
-    
-    return model
-
-
-# Removing Menu
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(hide_streamlit_style, unsafe_allow_html = True)
 
-# Loading the Model
-model = load_model('model.h5')
+st.title('POTATO PLANT DISEASE PREDICTION')
 
-# Title and Description
-st.title('Plant Diesease Detection')
-st.write("Just Upload your Plant's Leaf Image and get predictions if the plant is healthy or not")
+def main() :
+    file_uploaded = st.file_uploader('Choose an image...', type = 'jpg')
+    if file_uploaded is not None :
+        image = Image.open(file_uploaded)
+        st.write("Uploaded Image.")
+        figure = plt.figure()
+        plt.imshow(image)
+        plt.axis('off')
+        st.pyplot(figure)
+        result, confidence = predict_class(image)
+        st.write('Prediction : {}'.format(result))
+        st.write('Confidence : {}%'.format(confidence))
 
-# Setting the files that can be uploaded
-uploaded_file = st.file_uploader("Choose a Image file", type=["png", "jpg"])
+def predict_class(image) :
+    with st.spinner('Loading Model...'):
+        classifier_model = keras.models.load_model(r'potato_model.h5', compile = False)
 
-# If there is a uploaded file, start making prediction
-if uploaded_file != None:
-    
-    # Display progress and text
-    progress = st.text("Crunching Image")
-    my_bar = st.progress(0)
-    i = 0
-    
-    # Reading the uploaded image
-    image = Image.open(io.BytesIO(uploaded_file.read()))
-    st.image(np.array(Image.fromarray(
-        np.array(image)).resize((700, 400), Image.ANTIALIAS)), width=None)
-    my_bar.progress(i + 40)
-    
-    # Cleaning the image
-    image = clean_image(image)
-    
-    # Making the predictions
-    predictions, predictions_arr = get_prediction(model, image)
-    my_bar.progress(i + 30)
-    
-    # Making the results
-    result = make_results(predictions, predictions_arr)
-    
-    # Removing progress bar and text after prediction done
-    my_bar.progress(i + 30)
-    progress.empty()
-    i = 0
-    my_bar.empty()
-    
-    # Show the results
-    st.write(f"The plant {result['status']} with {result['prediction']} prediction.")
+    shape = ((256,256,3))
+    model = keras.Sequential([hub.KerasLayer(classifier_model, input_shape = shape)])     
+    test_image = image.resize((256, 256))
+    test_image = keras.preprocessing.image.img_to_array(test_image)
+    test_image /= 255.0
+    test_image = np.expand_dims(test_image, axis = 0)
+    class_name = ['Potato__Early_blight', 'Potato__Late_blight', 'Potato__healthy']
 
+    prediction = model.predict(test_image)
+    confidence = round(100 * (np.max(prediction[0])), 2)
+    final_pred = class_name[np.argmax(prediction)]
+    return final_pred, confidence
+
+footer = """<style>
+a:link , a:visited{
+    color: black;
+    background-color: transparent ;
+    text-decoration: none;
+}
+
+a:hover,  a:active {
+    color: red;
+    background-color: transparent;
+    text-decoration: none;
+}
+
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: transparent;
+    color: black;
+    text-align: center;
+}
+</style>
+
+<div class="footer">
+<p align="center"> <a href="https://github.com/ilegnaa"> Developed with care 😃 </a></p>
+</div>
+        """
+
+st.markdown(footer, unsafe_allow_html = True)
+
+if __name__ == '__main__' :
+    main()
